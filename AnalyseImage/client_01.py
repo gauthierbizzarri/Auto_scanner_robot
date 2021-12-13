a67c4de21e7de2d2facf2fe66543952f7a40283b
Gauthier BIZZARRI <gauthier-tristant.bizzarri@imerir.com>
2021-12-13 15:14:50 +0100

    [FIX] sendin QR code valid automatisation

--- AnalyseImage/client_01.py
+++ AnalyseImage/client_01.py
@@ -4,17 +4,24 @@ import sys
 import time
 import cv2
 import imutils
+import json
 import numpy as np
 from paho.mqtt import client as mqtt_client
+import color_detection
 
+
+id_camera = 3 ##num groupe
 
 broker = 'broker.emqx.io'
+
 port = 1883
-topic_QR_ = "python/mqtt"
+
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 username = 'emqx'
 password = 'public'
+
+ID_ROBOT = "ROBOT3"
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
@@ -30,26 +37,47 @@ def connect_mqtt():
     return client
 
 
-def publish(client,message_recu):
-    message_recu = message_recu
-    msg = f"messages: {message_recu}"
-    result = client.publish(topic_QR_, msg)
-    # result: [0, 1]
-    status = result[0]
-    if status == 0:
-        print(f"Send `{msg}` to topic `{topic_QR_}`")
-    else:
-        print(f"Failed to send message to topic {topic}")
+def publish(client,action=None,id_robot=None):
+    while(True):
+        if action =="QR_CODE":
+            topic = "field/camera/{}/scan".format(id_camera)
+            result = client.publish(topic, id_robot)
+            status = result[0]
+            if status == 0:
+                data_set = {"robot": id_robot}
+                json_dump = json.dumps(data_set)
+                print(f"Send {json_dump}to topic `{topic_QR_}`")
+            else:
+                print(f"Failed to send message to topic {topic_QR_}")
 
 
 def run():
     client = connect_mqtt()
     client.loop_start()
-    while True :
-        status , message = QR()
-        if status:
-            publish(client,message)
+    response = []
+    cap = cv2.VideoCapture(4)
+    cap.set(4, 640)
+    cap.set(4, 480)
+    while(True):
+        #time.sleep(1)
+        ret, frame = cap.read()
+        cv2.imshow("frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+        
+        
+        #example code from google TODO: link
+        qr_decoder = cv2.QRCodeDetector()
 
+        # Detect and decode the qrcode
+        data, bbox, rectified_image = qr_decoder.detectAndDecode(frame)
+        if len(data)>0 and data ==ID_ROBOT:
+           topic = "field/camera/{}/scan".format(id_camera)
+           data_set = {"robot": data}
+           json_dump = json.dumps(data_set)
+           result = client.publish(topic, json_dump)
+           status = result[0]
+           if status == 0:
+                print(f"Send {json_dump}to topic `{topic}`")
 
 
     # Display barcode and QR code location
@@ -63,28 +91,27 @@ def display(im, bbox):
 def QR():
     response = []
     cap = cv2.VideoCapture(4)
-    time.sleep(1)
-
-    while True:
-        ret, frame = cap.read()
-        cv2.imshow("frame", frame)
-        key = cv2.waitKey(1) & 0xFF
-        
-        
-        #example code from google TODO: link
-        qr_decoder = cv2.QRCodeDetector()
+    cap.set(3, 640)
+    cap.set(4, 480)
+    #time.sleep(1)
+    ret, frame = cap.read()
+    cv2.imshow("frame", frame)
+    key = cv2.waitKey(1) & 0xFF
     
-        # Detect and decode the qrcode
-        data, bbox, rectified_image = qr_decoder.detectAndDecode(frame)
-        if len(data)>0:
-            print("Decoded Data : {}".format(data))
-            response.append(True)
-            response.append(data)
-            return response
-            #rectified_image = np.uint8(rectified_image);
-            #cv2.imshow("Rectified QRCode", rectified_image);
-        else:
-            pass
+    
+    #example code from google TODO: link
+    qr_decoder = cv2.QRCodeDetector()
+
+    # Detect and decode the qrcode
+    data, bbox, rectified_image = qr_decoder.detectAndDecode(frame)
+    if len(data)>0 and data ==ID_ROBOT:
+        response.append(True)
+        response.append(data)
+        return response
+        #rectified_image = np.uint8(rectified_image);
+        #cv2.imshow("Rectified QRCode", rectified_image);
+    else:
+        pass
     if cv2.waitKey(10) & 0xFF == ord('q'):
         cap.release()
         cv2.destroyAllWindows()
